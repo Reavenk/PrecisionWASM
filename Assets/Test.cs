b1bc9557caa3509e2ffa25d1b42006bc7da75af5
Reavenk <Reavenk@gmail.com>
2021-02-23 00:32:38 -0600

    Added extra test
    The full set of samples from the wat2wasm demo

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -13,9 +13,18 @@ public class Test : MonoBehaviour
 
     public UnitTest [] test;
 
+    // Some tests. This will probably eventually be converted
+    // into formal (NUnit) unit tests when things get mature.
+    // (wleu 02/22/2021)
     public string loadTargetEmpty;
     public string loadTargetSimple;
     public string loadTargetFactorial;
+    public string loadTargetStuff;
+    public string loadTargetMutableGlobals;
+    public string loadTargetSaturatingFloatToInt;
+    public string loadTargetSignExtension;
+    public string loadTargetMultiValue;
+    public string loadTargetBulkMemory;
 
     [Multiline(10)]
     public string testParse;
@@ -34,7 +43,11 @@ public class Test : MonoBehaviour
 
     private void OnGUI()
     {
-        if(GUILayout.Button("Test Binary Empty") == true)
+        //
+        // Tests and binaries for them at https://webassembly.github.io/wabt/demo/wat2wasm/
+        //
+
+        if (GUILayout.Button("Test Binary Empty") == true)
         {
             byte[] rb = System.IO.File.ReadAllBytes(this.loadTargetSimple);
             this.session = PxPre.WASM.Module.LoadBinary(rb);
@@ -76,9 +89,46 @@ public class Test : MonoBehaviour
                 Debug.Log(values[0].GetString());
         }
 
+        if(GUILayout.Button("Test Binary stuff") == true)
+        {
+            byte[] rb = System.IO.File.ReadAllBytes(this.loadTargetStuff);
+            this.session = PxPre.WASM.Module.LoadBinary(rb);
 
+            PxPre.WASM.ExecutionContext ex = new PxPre.WASM.ExecutionContext();
+            ex.InvokeStart(this.session, true);
+        }
 
-        if(GUILayout.Button("Parse") == true)
+        if(GUILayout.Button("Test Binary mutable globals") == true)
+        {
+            byte[] rb = System.IO.File.ReadAllBytes(this.loadTargetMutableGlobals);
+            this.session = PxPre.WASM.Module.LoadBinary(rb);
+        }
+
+        if(GUILayout.Button("Test Binary saturating float-to-int") == true)
+        {
+            byte[] rb = System.IO.File.ReadAllBytes(this.loadTargetSaturatingFloatToInt);
+            this.session = PxPre.WASM.Module.LoadBinary(rb);
+        }
+
+        if(GUILayout.Button("Test Binary sign extension") == true)
+        {
+            byte[] rb = System.IO.File.ReadAllBytes(this.loadTargetSignExtension);
+            this.session = PxPre.WASM.Module.LoadBinary(rb);
+        }
+
+        if(GUILayout.Button("Test Binary multi value") == true)
+        {
+            byte[] rb = System.IO.File.ReadAllBytes(this.loadTargetMultiValue);
+            this.session = PxPre.WASM.Module.LoadBinary(rb);
+        }
+
+        if (GUILayout.Button("Test Binary bulk memory") == true)
+        {
+            byte[] rb = System.IO.File.ReadAllBytes(this.loadTargetBulkMemory);
+            this.session = PxPre.WASM.Module.LoadBinary(rb);
+        }
+
+        if (GUILayout.Button("Parse") == true)
         { 
             byte [] rb = PxPre.WASM.Parser.ParseAndCompile(testParse);
             Debug.Log("Parsed!");
